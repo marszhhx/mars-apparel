0437a47d519f282f215b94add0ad37859e7835e6
marszhhx <starzhang0755@gmail.com>
2023-10-02 16:37:58 -0700

    added sign in component and brought in firebase libraries

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import Navigation from './components/routes/navigation/navigation.component';
 import Home from './components/routes/home/home.component';
+import SignIn from './components/routes/sign-in/sign-in.component';
 
 const Shop = () => {
   return <h1>Shop page</h1>;
@@ -10,9 +11,10 @@ const Shop = () => {
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigation />}>
+      <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="shop" element={<Shop />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   );
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,67 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBIjzUqy-W85NLLrIlmlSAVPYPZ8W7zPkU',
+  authDomain: 'mars-apparel-db.firebaseapp.com',
+  projectId: 'mars-apparel-db',
+  storageBucket: 'mars-apparel-db.appspot.com',
+  messagingSenderId: '1030375418654',
+  appId: '1:1030375418654:web:474eba06a66a054be3770f',
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapShot = await getDoc(userDocRef);
+  console.log(userSnapShot);
+  console.log(userSnapShot.exists());
+
+  // if user data does not exist
+  // create / set the document with the data from userAuth in my collection
+
+  if (!userSnapShot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  // if user data exists
+  // return userDocRef
+  return userDocRef;
+};
